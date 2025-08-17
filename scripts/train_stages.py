49a4e433963ef89b24700251448b06f6f3c79f0b
dterrero <dicksonterrero1@hotmail.com>
2025-08-17 12:43:33 -0400

    Update train_stages.py

--- scripts/train_stages.py
+++ scripts/train_stages.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0
+# Copyright (c) 2025 Dickson A Terrero
+
 import argparse, json, math
 from pathlib import Path
 import numpy as np, pandas as pd
